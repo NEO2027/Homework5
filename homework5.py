785492c1c258801c2506a5b380676363dcfd1eaf
NEO2027 <art.lyakhov07@gmail.com>
2024-11-30 18:24:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+my_list = ["apple", "cherry" "banana", "orange", "strawberry"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = "pineapple"
+print(my_list)
+
+my_dict = {"sun": "солнце", "moon": "луна", "house": "дом"}
+print(my_dict)
+key = "moon"
+print(my_dict[key])
+my_dict["house"] = "здание"
+my_dict["car"] = "машина"
+print(my_dict)
